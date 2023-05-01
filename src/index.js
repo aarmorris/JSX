1c666ad38f6b1d5ca11b76ef501c5ee1bc5b0d9b
Aaron Morris <aaron.r.morris3@gmail.com>
2023-05-01 17:02:48 -0500

    Printing JavaScript Variables in JSX

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@
 // 2) Get a reference to the div with ID root
     const el = document.getElementById('root');
 
-
 // 3) Tell React to take control of that element
-const root = ReactDOM.createRoot(el);
+        const root = ReactDOM.createRoot(el);
 // 4) Create a component
     function App(){
-        return <h1>Hi there!</h1>;
+        let message = 'Bye there!'
+        if (Math.random() > 0.5) {
+            message ='Hello there!'
+        }
+        return <h1>{message}</h1>;
     }
 // 5) Show the component on the screen
 root.render(<App/>);
