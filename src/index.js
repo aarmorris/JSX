a90aaf2b3567cb945c41dd5619ce6721225373b2
Aaron Morris <aaron.r.morris3@gmail.com>
2023-05-02 13:57:28 -0500

    Typical Component Layouts

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@
         const root = ReactDOM.createRoot(el);
 // 4) Create a component
     function App(){
-         const date = new Date();
-         const time = date.toLocaleTimeString();
+        const name = 'Aaron';
+        const age = 23;
 
 
-        return <h1>{time}</h1>;
+        return <h1>Hi, my name is {name} and my age is {age} </h1>;
     }
 // 5) Show the component on the screen
 root.render(<App/>);
